914629cf781066682232275c4b98a656d497120d
PJY <winseawoo@naver.com>
2023-05-02 23:09:42 +0900

    First Commit

---
+++ app/src/main/java/com/example/project01/DashboardFragment.java
@@ -0,0 +1,16 @@
+package com.example.project01;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.customtablayout.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment(){
+
+        }
+
+public class DashboardFragment {
+}
+q
